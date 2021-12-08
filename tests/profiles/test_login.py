df486b791c2fc0daa8e475d464596f22a147939e
Clovis <clovis@dnclovis.com>
2021-12-08 14:59:21 -0600

    Update lock file

--- tests/profiles/test_login.py
+++ tests/profiles/test_login.py
@@ -1,5 +1,7 @@
+from django.urls import reverse
 
 
-def test_test_init(user_alice):
-    assert user_alice.email == 'alice@abc.com'
-    assert user_alice.username == 'alice@abc.com'
+class TestLoginView:
+    def test_init(self, user_alice):
+        assert user_alice.email == 'alice@abc.com'
+        assert user_alice.username == 'alice@abc.com'
