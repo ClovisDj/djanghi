3843f6307fc65c44cd29eee5fb8806a6b86d817b
Clovis <clovis@dnclovis.com>
2022-02-18 18:01:22 -0600

    Add MemberContributionFieldAdmin

--- apps/associations/admin.py
+++ apps/associations/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from apps.associations.models import Association
+from apps.associations.models import Association, MemberContributionField
 
 
 class AssociationAdmin(admin.ModelAdmin):
     pass
 
 
+class MemberContributionFieldAdmin(admin.ModelAdmin):
+    list_display = ('name', 'association_label', )
+
+    @staticmethod
+    def association_label(obj):
+        return obj.association.label
+
+
 admin.site.register(Association, AssociationAdmin)
+
+admin.site.register(MemberContributionField, MemberContributionFieldAdmin)
