3adba23496693e01ed9217bf0829b289f90d3f6f
Clovis <diclovis@gmail.com>
2022-05-31 11:07:22 -0500

    Use environ variable in db entrypoint

--- entrypoints/init-database.sh
+++ entrypoints/init-database.sh
@@ -3,8 +3,8 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER docker;
-    CREATE DATABASE djanghi-db;
-    GRANT ALL PRIVILEGES ON DATABASE djanghi-db TO docker;
+    CREATE DATABASE "$POSTGRES_DB";
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO docker;
     CREATE DATABASE djanghi-test-db;
     GRANT ALL PRIVILEGES ON DATABASE djanghi-test-db TO docker;
 EOSQL
