1da47c0d24ca64bf17351b0068f2541568407d2b
Clovis <diclovis@gmail.com>
2022-06-09 18:56:35 -0500

    Config certbot for ssl

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -252,7 +252,8 @@ class UserRegistrationLink(CreateUpdateDateMixin, UUIDModelMixin, models.Model):
         ordering = ['-updated_at']
 
     def save(self, *args, **kwargs):
-        self.link = f'{settings.API_HOST}/activate/{str(self.association_id)}/{str(self.user_id)}/{str(self.id)}'
+        schema = 'http://' if settings.ENVIRONMENT.lower() == 'local' else 'https://'
+        self.link = f'{schema}{settings.API_HOST}/activate/{str(self.association_id)}/{str(self.user_id)}/{str(self.id)}'
 
         return super().save(*args, **kwargs)
 
