a6e5c6e9cbd235c5515d7bb47aff0986b20437e3
Clovis <diclovis@gmail.com>
2022-06-09 19:37:58 -0500

    Tweak settings

--- settings/base.py
+++ settings/base.py
@@ -181,7 +181,7 @@ GEOS_LIBRARY_PATH = glob('/usr/lib/libgeos_c.so.*')[0]
 DEFAULT_REGISTRATION_LINK_LIFE = 10     # 10 Days
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-if ENVIRONMENT.lower() != 'local':
+if ENVIRONMENT.lower() == 'prod':
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
